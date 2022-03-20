c108b8edfaf0b378d28b309c2f3dc263b24dd28c
SyedHasibRahman <syedhasib01@gmail.com>
2022-03-20 19:52:34 +0600

    fsPromises, appendFile and more

---
+++ 04TUT/index.js
@@ -0,0 +1,17 @@
+const logEvents = require('./logEvents');
+
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter { };
+
+//initialize object
+
+const myEmitter = new MyEmitter();
+
+// add listener for the log event
+myEmitter.on('log', (msg) => logEvents(msg));
+
+setTimeout(() => {
+    //Emit event
+    myEmitter.emit('log', 'Log event emitted!');
+}, 2000);   
